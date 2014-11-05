c0029e14becda94bc0220a8d4e78e9efa4f7cdd7
kalik <wargas_14@o2.pl>
2014-11-05 18:16:59 +0100

    Przesiadka na MVC5, potwierdzenie mailowe utworzenia konta

---
+++ Jedznaplus/Controllers/RecipesController.cs
@@ -0,0 +1,224 @@
+﻿using Jedznaplus.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jedznaplus.Controllers
+{
+    public class RecipesController : Controller
+    {
+        //
+        // GET: /Recipes/
+
+        RecipesDataContext db = new RecipesDataContext();
+
+        public ActionResult Index()
+        {
+         var recipes=db.Recipes.ToList();
+
+            return View(recipes);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Models.Recipe recipe, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {               
+                recipe.UserName = User.Identity.Name;
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    // extract only the fielname
+                    var fileName = Path.GetFileName(file.FileName);
+                    // store the file inside ~/App_Data/uploads folder
+                    var uniqueFileName =Guid.NewGuid()+ fileName;
+                    var absolutePath = Path.Combine(Server.MapPath("~/Images"), uniqueFileName);
+                    var relativePath = "~/Images/" + uniqueFileName;
+                    file.SaveAs(absolutePath);
+                    recipe.ImageUrl = relativePath;
+                }
+                else
+                {
+                    recipe.ImageUrl = "~/Images/noPhoto.png";
+                }
+                db.Recipes.Add(recipe);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return Create();
+        }
+
+      
+        [Authorize]
+        [HttpGet]
+        public ActionResult Edit (int ?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var recipe = db.Recipes.Find(id);
+            return View(recipe);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(Models.Recipe recipe, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbPost = db.Recipes.FirstOrDefault(p => p.Id == recipe.Id);
+                if (dbPost == null)
+                {
+                    return HttpNotFound();
+                }
+
+                dbPost.Calories = recipe.Calories;
+                dbPost.Ingredients = recipe.Ingredients;
+                dbPost.Name = recipe.Name;
+                dbPost.PreparationMethod = recipe.PreparationMethod;
+                dbPost.PreparationTime = recipe.PreparationTime;
+                dbPost.Serves = recipe.Serves;
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    // extract only the fielname
+                    var fileName = Path.GetFileName(file.FileName);
+                    // store the file inside ~/App_Data/uploads folder
+                    var uniqueFileName=Guid.NewGuid()+fileName;
+                    var absolutePath = Path.Combine(Server.MapPath("~/Images"), uniqueFileName);
+                    var relativePath = "~/Images/" + uniqueFileName;
+                    file.SaveAs(absolutePath);
+                    dbPost.ImageUrl = relativePath;
+                }
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recipe);
+
+        }
+
+
+        [Authorize]
+        public ActionResult Delete(int ?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var toDelete = db.Recipes.Find(id);
+            
+            if(toDelete!=null)
+            {
+                db.Recipes.Remove(toDelete);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult UserRecipes()
+        {
+            var userRecipes = db.Recipes.Where(a=>a.UserName== User.Identity.Name).ToList();
+
+            return View(userRecipes);
+        }
+
+        public ActionResult Details(int ?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var toView = db.Recipes.Find(id);
+            
+            if(toView!=null)
+            {
+                return View(toView);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteImage(int ?id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var toDelete = db.Recipes.Find(id);
+
+            if (toDelete != null)
+            {
+                var path=Server.MapPath(toDelete.ImageUrl);
+                System.IO.File.Delete(path);
+                toDelete.ImageUrl = "~/Images/noPhoto.png";
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Edit",db.Recipes.Find(id));
+
+        }
+
+        public ActionResult Search(string search)
+        {
+            var recipes = db.Recipes.Where(a => a.Name.ToLower() == search.ToLower()).ToList();
+
+            foreach(var rec in db.Recipes)
+            {
+                if(rec.Ingredients.IndexOf(search.ToLower(),StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    if(!recipes.Contains(rec))
+                    recipes.Add(rec);
+                }
+            }
+
+            return View(recipes);
+        }
+
+        [HttpGet]
+        public ActionResult AdvancedSearch()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AdvancedSearch(int? Time, string WithoutIngred, bool? Vegetarians)
+        {
+            if (Time == null) Time = 1000;
+            if (Vegetarians == null) Vegetarians = false;
+            if (string.IsNullOrEmpty(WithoutIngred)) WithoutIngred ="testowy string";
+            var recipes = new List<Recipe>();
+            foreach(var rec in db.Recipes)
+            {
+                if(rec.PreparationTime<=Time && (rec.Ingredients.IndexOf(WithoutIngred.ToLower(),StringComparison.OrdinalIgnoreCase)<0))
+                {
+                    recipes.Add(rec);
+                }
+            }
+            return View("Search",recipes);
+        }
+
+    }
+}
---
+++ Jedznaplus/Models/RecipesDataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Jedznaplus.Models
+{
+    public class RecipesDataContext : DbContext
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+
+             public RecipesDataContext()
+            : base("DefaultConnection")
+        {
+        }
+
+     /*   static RecipesDataContext()
+        {
+          Database.SetInitializer(new DropCreateDatabaseAlways<RecipesDataContext>());
+        }*/
+
+    }
+}
