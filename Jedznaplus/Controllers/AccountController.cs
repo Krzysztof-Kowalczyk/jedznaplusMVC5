2dda9116eed567f2fc5151166a4fca81d25195a6
kalik <wargas_14@o2.pl>
2014-11-18 21:00:29 +0100

    Blokada konta po kilku nie udanych probach

--- Jedznaplus/Controllers/AccountController.cs
+++ Jedznaplus/Controllers/AccountController.cs
@@ -77,7 +77,7 @@ namespace Jedznaplus.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Login, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Login, model.Password, model.RememberMe, shouldLockout: true);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -88,7 +88,7 @@ namespace Jedznaplus.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Wprowadzono błędną Nazwę użytkownika");
+                    ModelState.AddModelError("", "Wprowadzono błędną Nazwę użytkownika lub hasło");
                     return View(model);
             }
         }
